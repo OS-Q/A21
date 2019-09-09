b29002d886d25653188a3801038b285fdf404c61
qitas <qitas@qitas.cn>
2019-09-09 21:31:07 +0800

    fix

--- tools/platformio-build.py
+++ tools/platformio-build.py
@@ -1,28 +1,3 @@
-# Copyright 2014-present PlatformIO <contact@platformio.org>
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""
-Arduino
-
-Arduino Wiring-based Framework allows writing cross-platform software to
-control devices attached to a wide range of Arduino boards to create all
-kinds of creative coding, interactive objects, spaces or physical experiences.
-
-https://github.com/stm32duino/Arduino_Core_STM32
-"""
-
-
 from os.path import isfile, isdir, join
 
 from SCons.Script import DefaultEnvironment
@@ -31,9 +6,9 @@ env = DefaultEnvironment()
 platform = env.PioPlatform()
 board = env.BoardConfig()
 
-FRAMEWORK_DIR = platform.get_package_dir("framework-arduinoststm32")
+FRAMEWORK_DIR = platform.get_package_dir("framework-stm32arduino")
 CMSIS_DIR = join(platform.get_package_dir(
-    "framework-arduinoststm32"), "CMSIS", "CMSIS")
+    "framework-stm32arduino"), "CMSIS", "CMSIS")
 assert isdir(FRAMEWORK_DIR)
 assert isdir(CMSIS_DIR)
 
