f3d70d6ac16ae7298131362806037ca84f264b29
qitas <qitas@qitas.cn>
2019-12-07 16:43:43 +0800

    fix some bug

--- tools/platformio-build.py
+++ tools/platformio-build.py
@@ -1,17 +1,3 @@
-# Copyright 2014-present PlatformIO <contact@platformio.org>
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 """
 Arduino
 
@@ -31,9 +17,9 @@ env = DefaultEnvironment()
 platform = env.PioPlatform()
 board = env.BoardConfig()
 
-FRAMEWORK_DIR = platform.get_package_dir("framework-arduinoststm32")
+FRAMEWORK_DIR = platform.get_package_dir("framework-N06")
 CMSIS_DIR = join(platform.get_package_dir(
-    "framework-arduinoststm32"), "CMSIS", "CMSIS")
+    "framework-N06"), "CMSIS", "CMSIS")
 assert isdir(FRAMEWORK_DIR)
 assert isdir(CMSIS_DIR)
 
